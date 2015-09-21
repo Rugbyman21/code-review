82c5b3441a7b6e3cac7351b87bbb7bc2612e5eb2
Rugbyman21 <trevorscherling@Trevors-MacBook-Air.local>
2015-09-20 21:40:35 -0700

    adding code to work the html file

--- js/scripts.js
+++ js/scripts.js
@@ -13,12 +13,14 @@ Pizza.prototype.totalCost = function() {
   var sizeCost = 0;
 
 
-  if (this.size === "medium") {
-    sizeCost = 7;
-  } else if (this.size === "small") {
-    sizeCost = 5;
-  } else {
+  if (this.size === "large") {
     sizeCost = 10;
+  } else if (this.size === "medium") {
+    sizeCost = 7;
+  }  else if (this.size === "small") {
+      sizeCost = 5;
+  } else {
+    sizeCost = 12;
   }
 // debugger;
 
@@ -29,3 +31,40 @@ Pizza.prototype.totalCost = function() {
   return totalCost;
 
 };
+
+$(document).ready(function() {
+  $("form#pizzaOrder").submit(function(event) {
+    var costForAll = 0;
+    event.preventDefault();
+
+    $(".pizzaCreation").each(function() {
+    var pizzaTopping = $("input#topping").val().toString();
+    var size = $("select#size").val();
+    var quinity = $("input#quinityOfPizza").val();
+    var pizza1 = new Pizza(size, quinity, pizzaTopping);
+    $("#pizzaCostDisplay").text(quinity + " " + size + " " + "pizza with " + pizzaTopping + " " + "will cost $" + pizza1.totalCost());
+    costForAll += pizza1.totalCost();
+  });
+
+  $("#costForAllPizza").text("total cost for all pizza's will be $" + costForAll);
+    $("select#size").val("Select Size");
+    $("input#topping").val("");
+    $("input#quinityOfPizza").val("");
+
+  });
+
+ //  $("#addNewPizza").click(function(){
+ //   $("#pizzaForms").append("<div class='pizzaCreation'><label for='pizzaSize'>Pizza Size: </label>" + "<select id='size'>" +     "<option>Select Size</option>" +
+ //   "<option value='x-large'>X-Large</option>" +
+ //   "<option value='large'>Large</option>" +
+ //   "<option value='medium'>Medium</option>" +
+ //   "<option value='small'>Small</option>" +
+ //   "</select>" +
+ //   "<label for='topping'>Toppings</label>" +
+ //   "<input type='text' id='topping' placeholder='pepperoni, cheese, etc.'>" +
+ //   "<label for='numberOfPizza'>Number of Pizza's:</label>" +
+ //   "<input type='text' id='numberOfPizza'> </div>");
+ // });
+
+
+});
